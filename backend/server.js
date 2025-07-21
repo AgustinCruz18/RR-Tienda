46ef76560794901691132e414bd2507238746a5a
AgustinCruz18 <agustin45880644@gmail.com>
2025-07-21 00:54:07 -0300

    Implementar validación de email en el registro de usuarios

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,15 @@ const connectDB = require('./config/db');
 
 const app = express();
 connectDB();
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
-
 // Rutas
 app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/usuarios', require('./routes/usuario.routes'));
 app.use('/api/productos', require('./routes/producto.routes'));
 app.use('/api/ventas', require('./routes/venta.routes'));
 app.use('/api', require('./routes/upload.routes')); // /api/upload
-
 // Servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
